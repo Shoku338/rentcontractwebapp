fa4885f0d16af1f2d2d5147a8280d807e74ca9b0
Kapom310 <124363815+Kapom310@users.noreply.github.com>
2025-11-12 15:45:57 +0700

    database connect

---
+++ app/api/newTenant/route.ts
@@ -0,0 +1,18 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+import { createClient } from '@/lib/supabase/server';
+import { cookies } from 'next/headers';
+
+export async function GET(req:NextRequest)
+{
+    const cookieStore = await cookies();
+    const supabase = createClient(cookieStore);
+
+    const {data: newestTenant, error} = await supabase. from('tenants').select('*');
+    if(error)
+    {
+        console.log('Error fetching newest tenant:', error);
+        return NextResponse.json({error: `Error fetching newest tenant: ${error.message}`}, {status: 500});
+    }
+    return NextResponse.json(newestTenant);
+}
