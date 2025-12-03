25ef612a7f33dcabd32e0d5200c2555b0c2c96f8
Shoku338 <surfer.suvijak@gmail.com>
2025-12-03 12:03:47 +0700

    Update route.ts

--- app/api/newTenant/route.ts
+++ app/api/newTenant/route.ts
@@ -1,18 +1,25 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { createClient } from '@/lib/supabase/server';
-import { cookies } from 'next/headers';
+import { createServerClient } from '@supabase/ssr'
+import { cookies } from 'next/headers'
 
-export async function GET(req: NextRequest) {
-  const cookieStore = cookies();
-  const supabase = createClient(cookieStore);
+export const createClient = () => {
+  const cookieStore = cookies()
 
-  const { data: newestTenant, error } = await supabase.from('tenants').select('*');
-  if (error) {
-    console.log('Error fetching newest tenant:', error);
-    return NextResponse.json(
-      { error: `Error fetching newest tenant: ${error.message}` },
-      { status: 500 }
-    );
-  }
-  return NextResponse.json(newestTenant);
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll()
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookieStore.set(cookiesToSet)
+          } catch (error) {
+            console.error('Error setting cookies:', error)
+          }
+        },
+      },
+    }
+  )
 }
