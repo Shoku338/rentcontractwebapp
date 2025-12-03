cf094ea6341b3be8403978cb52a91dbb28a8d54a
Shoku338 <surfer.suvijak@gmail.com>
2025-12-03 12:12:17 +0700

    Update route.ts

--- app/api/newTenant/route.ts
+++ app/api/newTenant/route.ts
@@ -1,7 +1,8 @@
 import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
+import { NextRequest, NextResponse } from 'next/server'
 
-export const createClient = () => {
+const createClient = () => {
   const cookieStore = cookies()
 
   return createServerClient(
