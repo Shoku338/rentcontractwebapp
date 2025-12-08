264a6f34ce5264591912768e6d7ffc58da166800
Shoku338 <surfer.suvijak@gmail.com>
2025-12-09 02:41:22 +0700

    add admin tool

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,10 +1,15 @@
 import Toolbar from "@/components/Toolbar";
+import { getUserWithRole } from "@/app/api/User/route";
 
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+export default async function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const user = await getUserWithRole();
   return (
     <>
-      <Toolbar />
-      
+      <Toolbar userRole={user?.role} />
       <main>{children}</main>
     </>
   );
